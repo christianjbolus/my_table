1b42973411c47acc6898f6997d3030c71bec2a13
christianjbolus <59423548+christianjbolus@users.noreply.github.com>
2021-09-11 10:25:38 -0400

    Change target of build to es5 in tsconfig

--- dist/app.js
+++ dist/app.js
@@ -8,42 +8,93 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
+var __generator = (this && this.__generator) || function (thisArg, body) {
+    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
+    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
+    function verb(n) { return function (v) { return step([n, v]); }; }
+    function step(op) {
+        if (f) throw new TypeError("Generator is already executing.");
+        while (_) try {
+            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
+            if (y = 0, t) op = [op[0] & 2, t.value];
+            switch (op[0]) {
+                case 0: case 1: t = op; break;
+                case 4: _.label++; return { value: op[1], done: false };
+                case 5: _.label++; y = op[1]; op = [0]; continue;
+                case 7: op = _.ops.pop(); _.trys.pop(); continue;
+                default:
+                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
+                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
+                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
+                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
+                    if (t[2]) _.ops.pop();
+                    _.trys.pop(); continue;
+            }
+            op = body.call(thisArg, _);
+        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
+        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
+    }
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = __importDefault(require("express"));
-const cors_1 = __importDefault(require("cors"));
-const bcrypt_1 = __importDefault(require("bcrypt"));
-const api_1 = require("./services/api");
-const middleware_1 = require("./middleware/middleware");
-const helpers_1 = require("./utils/helpers");
-const PORT = process.env.PORT || 8080;
-const app = express_1.default();
+var express_1 = __importDefault(require("express"));
+var cors_1 = __importDefault(require("cors"));
+var bcrypt_1 = __importDefault(require("bcrypt"));
+var api_1 = require("./services/api");
+var middleware_1 = require("./middleware/middleware");
+var helpers_1 = require("./utils/helpers");
+var PORT = process.env.PORT || 8080;
+var app = express_1.default();
 app.use(cors_1.default());
 app.use(express_1.default.json());
-app.post('/register', middleware_1.isUsernameTaken, (req, res) => __awaiter(void 0, void 0, void 0, function* () {
-    try {
-        const { username, password } = req.body;
-        const hash = yield bcrypt_1.default.hash(password, 12);
-        const newUser = yield api_1.createUser({ username, password: hash });
-        res.send(newUser.id);
-    }
-    catch (err) {
-        res.status(409).send(err.message);
-    }
-}));
-app.post('/login', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
-    try {
-        const { username, password } = req.body;
-        const user = yield helpers_1.findUser(username);
-        const isValidPassword = yield bcrypt_1.default.compare(password, user.fields.password);
-        if (!isValidPassword)
-            throw new Error('Username or password is incorrect');
-        res.send(user.id);
-    }
-    catch (err) {
-        res.status(401).send(err.message);
-    }
-}));
-app.listen(PORT, () => console.log('Server running'));
+app.post('/register', middleware_1.isUsernameTaken, function (req, res) { return __awaiter(void 0, void 0, void 0, function () {
+    var _a, username, password, hash, newUser, err_1;
+    return __generator(this, function (_b) {
+        switch (_b.label) {
+            case 0:
+                _b.trys.push([0, 3, , 4]);
+                _a = req.body, username = _a.username, password = _a.password;
+                return [4 /*yield*/, bcrypt_1.default.hash(password, 12)];
+            case 1:
+                hash = _b.sent();
+                return [4 /*yield*/, api_1.createUser({ username: username, password: hash })];
+            case 2:
+                newUser = _b.sent();
+                res.send(newUser.id);
+                return [3 /*break*/, 4];
+            case 3:
+                err_1 = _b.sent();
+                res.status(409).send(err_1.message);
+                return [3 /*break*/, 4];
+            case 4: return [2 /*return*/];
+        }
+    });
+}); });
+app.post('/login', function (req, res) { return __awaiter(void 0, void 0, void 0, function () {
+    var _a, username, password, user, isValidPassword, err_2;
+    return __generator(this, function (_b) {
+        switch (_b.label) {
+            case 0:
+                _b.trys.push([0, 3, , 4]);
+                _a = req.body, username = _a.username, password = _a.password;
+                return [4 /*yield*/, helpers_1.findUser(username)];
+            case 1:
+                user = _b.sent();
+                return [4 /*yield*/, bcrypt_1.default.compare(password, user.fields.password)];
+            case 2:
+                isValidPassword = _b.sent();
+                if (!isValidPassword)
+                    throw new Error('Username or password is incorrect');
+                res.send(user.id);
+                return [3 /*break*/, 4];
+            case 3:
+                err_2 = _b.sent();
+                res.status(401).send(err_2.message);
+                return [3 /*break*/, 4];
+            case 4: return [2 /*return*/];
+        }
+    });
+}); });
+app.listen(PORT, function () { return console.log('Server running'); });
